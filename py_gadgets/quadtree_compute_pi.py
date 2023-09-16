223db57892a5ceabdbc05c32b003daea866e8033
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-09-16 11:41:16 -0700

    Optimize algorithm
    Algorithm now calculates 1/4 of the circle instead of the whole thing.

--- py_gadgets/quadtree_compute_pi.py
+++ py_gadgets/quadtree_compute_pi.py
@@ -5,12 +5,12 @@
 import math
 CANVAS_SIZE = 1
 precision = 10
-circle_x = circle_y = CANVAS_SIZE/2
+circle_x = circle_y = CANVAS_SIZE
 def dist(x1, y1, x2, y2):
     return math.sqrt((x1-x2)**2+(y1-y2)**2)
 
 def in_circle(x, y):
-    return dist(x, y, circle_x, circle_y) <= CANVAS_SIZE/2
+    return dist(x, y, circle_x, circle_y) < CANVAS_SIZE
 
 def rect_in_circle(x1, y1, x2, y2):
     return in_circle(x1, y1) != in_circle(x2, y1) or in_circle(x1, y1) != in_circle(x1, y2) or in_circle(x1, y2) != in_circle(x2, y2)
@@ -26,5 +26,7 @@ def subdivide_area(x1, y1, x2, y2, precision, start = True):
         area_sum += 0.25*subdivide_area(x1, ym, xm, y2, precision-1, False)
         return area_sum
     return float(in_circle(xm, ym))
+for i in range(1, 24):
+    print(f"{i} - {subdivide_area(0, 0, CANVAS_SIZE, CANVAS_SIZE, i)*4}")
 
-print(subdivide_area(0, 0, CANVAS_SIZE, CANVAS_SIZE, 15)*4)
+
